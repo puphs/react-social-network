645674c742bfc09f8eea85666dc5793aaf86c68e
puphs <43395605+puphs@users.noreply.github.com>
2020-12-20 11:42:07 +0400

    change ul li to div

--- src/Sidebar/Navbar/Navbar.jsx
+++ src/Sidebar/Navbar/Navbar.jsx
@@ -4,33 +4,36 @@ import s from './Navbar.module.css';
 const Navbar = () => {
 	return (
 		<nav className={s.navbar}>
-			<ul>
-				<li className={s.item}>
-					<NavLink className={`${s.itemLink}`} to="/profile" activeClassName={s.itemLink_active}>
-						Profile
-					</NavLink>
-				</li>
-				<li className={s.item}>
-					<NavLink className={s.itemLink} to="/dialogs" activeClassName={s.itemLink_active}>
-						Dialogs
-					</NavLink>
-				</li>
-				<li className={s.item}>
-					<NavLink className={s.itemLink} to="/news" activeClassName={s.itemLink_active}>
-						News
-					</NavLink>
-				</li>
-				<li className={s.item}>
-					<NavLink className={s.itemLink} to="/music" activeClassName={s.itemLink_active}>
-						Music
-					</NavLink>
-				</li>
-				<li className={s.item}>
-					<NavLink className={s.itemLink} to="/settings" activeClassName={s.itemLink_active}>
-						Settings
-					</NavLink>
-				</li>
-			</ul>
+			<div className={s.item}>
+				<NavLink className={`${s.itemLink}`} to="/profile" activeClassName={s.itemLink_active}>
+					Profile
+				</NavLink>
+			</div>
+			<div className={s.item}>
+				<NavLink className={s.itemLink} to="/dialogs" activeClassName={s.itemLink_active}>
+					Dialogs
+				</NavLink>
+			</div>
+			<div className={s.item}>
+				<NavLink className={s.itemLink} to="/news" activeClassName={s.itemLink_active}>
+					News
+				</NavLink>
+			</div>
+			<div className={s.item}>
+				<NavLink className={s.itemLink} to="/music" activeClassName={s.itemLink_active}>
+					Music
+				</NavLink>
+			</div>
+			<div className={s.item}>
+				<NavLink className={s.itemLink} to="/settings" activeClassName={s.itemLink_active}>
+					Settings
+				</NavLink>
+			</div>
+			<div className={s.item}>
+				<NavLink className={s.itemLink} to="/users" activeClassName={s.itemLink_active}>
+					Users
+				</NavLink>
+			</div>
 		</nav>
 	);
 };
