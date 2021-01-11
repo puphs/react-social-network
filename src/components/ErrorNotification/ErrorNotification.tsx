6a344c49ce5147b5481a560adcf784ba88b7e79a
puphs <43395605+puphs@users.noreply.github.com>
2021-01-11 11:31:56 +0400

    add types to ErrorNotification component

--- src/components/ErrorNotification/ErrorNotification.tsx
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -1,15 +1,21 @@
 import s from './ErrorNotification.module.css';
 import cn from 'classnames';
 import { connect } from 'react-redux';
+import { AppStateType } from '../../redux/reduxStore';
 
-const ErrorNotification = ({ error, shown }) => {
+type PropsType = {
+	error: string | null;
+	shown: boolean;
+};
+
+const ErrorNotification: React.FC<PropsType> = ({ error, shown }) => {
 	return (
 		<div className={cn(s.errorContainer, 'shadowBase', !shown && s.errorContainer_hidden)}>
 			<div className={s.errorMessage}>{error ?? 'Some error occured'}</div>
 		</div>
 	);
 };
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: AppStateType) => ({
 	shown: state.app.errorShown,
 	error: state.app.errorText,
 });
