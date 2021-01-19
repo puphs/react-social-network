218810dbb0e87f048fe21eed00f1a9700fbfce02
puphs <43395605+puphs@users.noreply.github.com>
2021-01-19 18:54:36 +0400

    create UsersSearchForm component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import s from './Users.module.css';
 import UserContainer from './User/UserContainer';
 import Pagination from '../Pagination/Pagination';
 import { UserType } from '../../types/types';
+import { Field, Form, Formik, FormikHelpers } from 'formik';
 
 type PropsType = {
 	users: Array<UserType>;
@@ -14,6 +15,7 @@ const Users: React.FC<PropsType> = ({ users, currentPage, totalUsersCount, pageS
 	const userElements = users.map((user) => <UserContainer user={user} key={user.id} />);
 	return (
 		<div className={s.container}>
+			<UsersSearchForm />
 			<div className={s.users}>{userElements}</div>
 			<Pagination
 				currentPage={currentPage}
@@ -22,6 +24,39 @@ const Users: React.FC<PropsType> = ({ users, currentPage, totalUsersCount, pageS
 				basePath={'users?page='}
 			/>
 		</div>
+	);
+};
+
+type UsersSearchFormValuesType = {
+	searchQuery: string;
+	friendsOnly: boolean;
+};
+
+const UsersSearchForm = () => {
+	const onSubmit = (
+		values: UsersSearchFormValuesType,
+		{ setSubmitting }: FormikHelpers<UsersSearchFormValuesType>
+	) => {
+		console.log(values);
+		setSubmitting(false);
+	};
+	return (
+		<Formik initialValues={{ searchQuery: '', friendsOnly: false }} onSubmit={onSubmit}>
+			{() => (
+				<Form className={s.searchForm}>
+					<Field placeholder={'Search with name'} className={s.searchInput} name={'searchQuery'} />
+					<Field
+						className={s.friendsOnlyInput}
+						id={'friendsOnly'}
+						name={'friendsOnly'}
+						type={'checkbox'}
+					/>
+					<label className={s.friendsOnlyLabel} htmlFor={'friendsOnly'}>
+						Friends only
+					</label>
+				</Form>
+			)}
+		</Formik>
 	);
 };
 
